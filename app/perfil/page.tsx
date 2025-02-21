fcb48d6f5e89bb18c1e7275ffebb530489d3bad5
Edson Ribeiro <edson.viniciussr@gmail.com>
2025-02-21 13:15:37 -0300

    feat: adiciona docker para ambiente "prod"

--- app/perfil/page.tsx
+++ app/perfil/page.tsx
@@ -1,11 +1,14 @@
 import PerfilForm from "@/components/base/PerfilForm";
+import { Suspense } from "react";
 
 export default function PerfilPage() {
   return (
     <div className="flex items-center justify-center min-h-screen">
       <div className="max-w-lg text-black w-full p-6 bg-white shadow-md rounded-lg bg-gray-50">
         <h2 className="text-2xl font-bold text-center mb-4">Perfil</h2>
-        <PerfilForm />
+        <Suspense>
+          <PerfilForm />
+        </Suspense>
       </div>
     </div>
   );
